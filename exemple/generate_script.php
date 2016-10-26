688251c1a2f0cd4b924702f42910fc1d74a2c04a
gegedenice <Geraldine.GEOFFROY@unice.fr>
2016-10-26 19:35:41 +0200

    Update generate_script.php

--- exemple/generate_script.php
+++ exemple/generate_script.php
@@ -1,18 +1,18 @@
 <?php
-include("php_fonctions.php");
-//On ouvre le fichier où va s'écrire le script (chemin relatif dans l'exemple)
+include("../php_fonctions.php");
+//On ouvre le fichier oÃ¹ va s'Ã©crire le script (chemin relatif dans l'exemple)
 $file = fopen("scripts_colodus_en_sortie.iim", "w");
-//On se logge à Colodus
+//On se logge Ã  Colodus
 loginColodus($file, "votre_login_colodus", "votre_mot_de_passe_colodus");
-/*On va chercher le fichier des ppn et données d'exemplaire de type
-PPN;Bibliothèque;Localisation;cote;CB;Numéro d'inventaire
+/*On va chercher le fichier des ppn et donnÃ©es d'exemplaire de type
+PPN;BibliothÃ¨que;Localisation;cote;CB;NumÃ©ro d'inventaire
 094665516;LASH;SL2;84/33 ROUS;0922155016;2016-DONS-LASH-304
 002887673;LASH;MAG2;;ML  24364;0922154325;2016-DONS-LASH-352
 001484311;LASH;SL2;842 SIM;0922155031;2016-DONS-LASH-366*/
 $source_file = "fichier_exemple_en_entree.txt";
 $contenu     = file_get_contents("$source_file");
 $notice      = explode("\r", $contenu);
-//On créé une boucle qui appelle la fonction getScriptCreate pour chaque ligne
+//On crÃ©Ã© une boucle qui appelle la fonction getScriptCreate pour chaque ligne
 foreach ($notice as $record) {
     $liste  = explode(';', $record);
     $ppn    = $liste[0];
